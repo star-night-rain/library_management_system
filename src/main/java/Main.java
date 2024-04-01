5ab3a40a886cb3c581ac292d8db8977a949e92dd
star-night-rain <1747432970.@qq.com>
2024-04-01 08:29:49 +0800

    初始化

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,8 @@
-package org.example;
-
+import ui.LoginJFrame;
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        new LoginJFrame();
     }
 }
